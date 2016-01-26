96b52302d007ef2ec42cce99e68ff16cd74306d8
Michal Cichra <michal@cichra.cz>
2016-01-26 21:27:07 +0100

    add configuration options

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ var spdy = require('spdy'),
 var dir = process.cwd()
 
 var defaultOptions = {
-  key: fs.readFileSync(dir + '/localhost.key'),
-  cert: fs.readFileSync(dir + '/localhost.crt'),
-  ca: fs.readFileSync(dir + '/localhost.crt'),
-
   spdy: {
     protocols: [ 'h2', 'spdy/3.1', 'http/1.1' ],
     plain: false,
@@ -18,33 +14,45 @@ var defaultOptions = {
       windowSize: 1024 * 1024, // Server's window size
     }
   }
+}
+
+var serverOptions = function(opts) {
+  var keys = {
+    key: fs.readFileSync(opts.key),
+    cert: fs.readFileSync(opts.cert),
+    ca: fs.readFileSync(opts.key)
+  }
+  return Object.assign({}, defaultOptions, opts, keys)
 };
 
 
 // Serve up public/ftp folder
-var serve = serveStatic(dir, {
-  'index': ['index.html', 'index.htm'],
-  'setHeaders': function(res, path, stat) {
-    if (path.includes('/jspm_packages/')) {
-      res.setHeader('Cache-Control', 'public, max-age=36000')
-      res.setHeader('Expires',' Thu, 15 Apr 2020 20:00:00 GMT')
-    } else {
-      res.setHeader('Cache-Control', 'public, max-age=0')
+var serveMiddleware = function(options) {
+  return serveStatic(options.dir, {
+    'index': ['index.html', 'index.htm'],
+    'setHeaders': function(res, path, stat) {
+      if (path.includes('/jspm_packages/')) {
+        res.setHeader('Cache-Control', 'public, max-age=36000')
+        res.setHeader('Expires',' Thu, 15 Apr 2020 20:00:00 GMT')
+      } else {
+        res.setHeader('Cache-Control', 'public, max-age=0')
+      }
     }
-
-  }
-})
+  })
+}
 
 
 module.exports = function(opts) {
-  var options = Object.assign({}, defaultOptions, opts)
+  var options = serverOptions(opts || {})
+  var serve = serveMiddleware(options)
 
   var server = spdy.createServer(options, function(req, res) {
     var done = finalhandler(req, res)
     serve(req, res, done)
   });
 
-  require('chokidar-socket-emitter')({app: server, dir: dir})
+  var chokidar = { app: server, dir: dir, chokidar: options.chokidar }
+  require('chokidar-socket-emitter')(chokidar)
 
   return server
 }
