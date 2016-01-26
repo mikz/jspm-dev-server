10c4c9de53d0c630e829236f3f75bf8ead3acd5f
Michal Cichra <michal@cichra.cz>
2016-01-26 23:54:14 +0100

    implement the proxy support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var spdy = require('spdy'),
   fs = require('fs'),
   serveStatic = require('serve-static'),
-  finalhandler = require('finalhandler')
+  finalhandler = require('finalhandler'),
+  httpProxy = require('http-proxy'),
+  url    = require('url')
 
 var dir = process.cwd()
 
@@ -29,6 +31,7 @@ var serverOptions = function(opts) {
 // Serve up public/ftp folder
 var serveMiddleware = function(options) {
   return serveStatic(options.dir, {
+    fallthrough: !!options.proxy,
     'index': ['index.html', 'index.htm'],
     'setHeaders': function(res, path, stat) {
       if (path.includes('/jspm_packages/')) {
@@ -41,14 +44,33 @@ var serveMiddleware = function(options) {
   })
 }
 
+var proxy = httpProxy.createProxyServer({protocolRewrite: 'https:', autoRewrite: true});
 
 module.exports = function(opts) {
   var options = serverOptions(opts || {})
   var serve = serveMiddleware(options)
+  var target = options.proxy && url.parse(options.proxy)
+
+  proxy.on('proxyReq', function(proxyReq, req, res, proxyOptions) {
+    proxyReq.setHeader('X-Forwarded-Proto', 'https');
+    console.log(`${req.method} https://${req.headers.host}${req.url} => ${options.proxy}`)
+  })
+
+  proxy.on('proxyRes', function(proxyRes, req, res, proxyOptions) {
+    var location = proxyRes.headers['location']
+    if (location) {
+      var u = url.parse(location)
+      u.host = target.host
+      proxyRes.headers['location'] = url.format(u)
+    }
+  })
 
   var server = spdy.createServer(options, function(req, res) {
     var done = finalhandler(req, res)
-    serve(req, res, done)
+
+    serve(req, res, function() {
+      proxy.web(req, res, { target: options.proxy }, done)
+    });
   });
 
   var chokidar = { app: server, dir: dir, chokidar: options.chokidar }
